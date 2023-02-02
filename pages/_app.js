6c4b9cdf83fcbe88af5ea53b2a71d1b83570c1b9
sajjad <samaralishaikh212@gmail.com>
2023-02-03 01:18:15 +0530

    Initial commit from npx thirdweb create

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import "../styles/globals.css";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Mainnet;
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp
