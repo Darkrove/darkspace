1cf3c1b98d32e3585a754412973ae28d7aacbd42
sajjad <samaralishaikh212@gmail.com>
2023-02-11 19:59:51 +0530

    add: pwa & sw 📦

--- next.config.js
+++ next.config.js
@@ -1,10 +1,27 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
 const nextConfig = {
   reactStrictMode: true,
   basePath: "",
   images: {
-    domains: ["kitwind.io","cdn.discordapp.com","avatars.githubusercontent.com","lh3.googleusercontent.com","ipfs.io","gateway.ipfs.io","cloudflare-ipfs.com","gateway.pinata.cloud","ipfs.eternum.io","dweb.link"],
+    domains: [
+      "kitwind.io",
+      "cdn.discordapp.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "ipfs.io",
+      "gateway.ipfs.io",
+      "cloudflare-ipfs.com",
+      "gateway.pinata.cloud",
+      "ipfs.eternum.io",
+      "dweb.link",
+    ],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
