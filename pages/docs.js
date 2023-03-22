986105d8c069feeca711c0db8dc00ba94a984cca
sajjad <samaralishaikh212@gmail.com>
2023-03-22 20:10:38 +0530

    add: doc page 👑

--- pages/docs.js
+++ pages/docs.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const docs = () => {
   return (
-    <div className="relative overflow-hidden isolate">
-      <div className="flex flex-col justify-center items-center space-y-2 text-center p-10 mt-[85px] h-screen">
+    <div className="relative overflow-hidden isolate h-screen flex justify-center items-cente">
+      <div className="flex flex-col justify-center items-center space-y-2 max-w-2xl text-center p-10 mt-[85px]">
         <h1 className="text-zinc-200 leading-none mb-3 text-[2.5rem] font-extrabold">
           Comming Soon ⏱
         </h1>
